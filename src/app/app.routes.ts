155e943f70e43fb49d2b29e1d1fc6e9c4ae66113
gplymenos <74688221+gplymenos@users.noreply.github.com>
2024-05-13 11:44:08 +0300

    FCB-17 header and sidenav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,93 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutUsComponent } from './routes/about-us/about-us.component';
+import { AdminComponent } from './routes/admin/admin.component';
+import { BookingsComponent } from './routes/admin/bookings/bookings.component';
+import { ClassesComponent } from './routes/admin/classes/classes.component';
+import { UsersComponent } from './routes/admin/users/users.component';
+import { ErrorPageComponent } from './routes/error-page/error-page.component';
+import { ProfileComponent } from './routes/profile/profile.component';
+import { ScheduleComponent } from './routes/schedule/schedule.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    component: HomepageComponent,
+    data: {
+      showInNav: true,
+      icon: 'home',
+    },
+  },
+  {
+    path: 'about-us',
+    title: 'About Us',
+    component: AboutUsComponent,
+    data: {
+      showInNav: true,
+      icon: 'sports_gymnastics',
+    },
+  },
+  {
+    path: 'profile',
+    title: 'My Profile',
+    component: ProfileComponent,
+    data: {
+      showInNav: true,
+      icon: 'person',
+    },
+  },
+  {
+    path: 'admin',
+    title: 'Admin',
+    data: {
+      showInNav: true,
+      icon: 'apps',
+    },
+    children: [
+      { path: '', component: AdminComponent },
+      {
+        path: 'users',
+        title: 'Manage Users',
+        component: UsersComponent,
+        data: {
+          showInNav: true,
+          icon: 'profile',
+        },
+      },
+      {
+        path: 'classes',
+        title: 'Manage Classes',
+        component: ClassesComponent,
+        data: {
+          showInNav: true,
+          icon: 'profile',
+        },
+      },
+      {
+        path: 'bookings',
+        title: 'Manage Bookings',
+        component: BookingsComponent,
+        data: {
+          showInNav: true,
+          icon: '',
+        },
+      },
+    ],
+  },
+  {
+    path: 'schedule',
+    title: 'Schedule',
+    component: ScheduleComponent,
+    data: {
+      showInNav: true,
+      icon: 'calendar_month',
+    },
+  },
+  {
+    path: 'not-found',
+    component: ErrorPageComponent,
+    data: { message: 'Page not found' },
+  },
+  { path: '**', redirectTo: '/not-found' },
+];
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -2,23 +2,60 @@ import {
   AfterViewInit,
   Component,
   EventEmitter,
+  OnInit,
   Output,
   ViewChild,
 } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { ActivatedRoute, RouterModule, Routes } from '@angular/router';
+import { AuthService } from '@g.plymenos/ng-firebase-auth';
+import firebase from 'firebase/compat';
+import { Subscription } from 'rxjs';
+import { routes } from '../app.routes';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatSidenavModule],
+  imports: [MatSidenavModule, RouterModule, MatIcon],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
 })
-export class SidenavComponent implements AfterViewInit {
+export class SidenavComponent implements OnInit, AfterViewInit {
   @Output('drawer') drawer = new EventEmitter();
   @ViewChild('drawerEl') drawerEl: MatDrawer;
+  routes: Routes;
+  loggedUserSubscription: Subscription;
+  loggedInUser: firebase.User | null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.loggedUserSubscription = this.authService
+      .getLoggedUserUpdates()
+      .subscribe((user) => {
+        if (user) {
+          // User is logged in
+          this.loggedInUser = user;
+        } else {
+          // User is not logged in or has logged out
+          this.loggedInUser = null;
+        }
+      });
+
+    this.routes = routes.filter(
+      (route) => route.data && route.data['showInNav']
+    );
+  }
 
   ngAfterViewInit(): void {
     this.drawer.emit(this.drawerEl);
+  }
+
+  logout() {
+    this.authService.signOut();
   }
 }
